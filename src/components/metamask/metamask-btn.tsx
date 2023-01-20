aaa2c344a0a74a22220d3b9a656043e47b6118d2
pholophus <iqbalkisas6@gmail.com>
2023-01-20 18:33:38 +0800

    first commit

---
+++ src/components/metamask/metamask-btn.tsx
@@ -0,0 +1,94 @@
+import { useState, useEffect } from 'react';
+import { ethers } from 'ethers';
+
+const MetaBtn = () => {
+  const [haveMetamask, sethaveMetamask] = useState(true);
+  const [accountAddress, setAccountAddress] = useState('');
+  const [accountBalance, setAccountBalance] = useState('');
+  const [currentAccount, setCurrentAccount] = useState("");  
+  const [isConnected, setIsConnected] = useState(false);
+
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const { ethereum } = window;
+
+  const checkIfAccountChanged = async () => {
+    try {
+      const {ethereum} = window;
+      ethereum.on('accountsChanged', (accounts: any) => {
+        console.log("Account changed to:", accounts[0]);
+        setCurrentAccount(accounts[0]);
+      });
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    const checkMetamaskAvailability = async () => {
+      if (!ethereum) {
+        sethaveMetamask(false);
+      }
+      sethaveMetamask(true);
+    };
+    checkMetamaskAvailability();
+    checkIfAccountChanged();
+  }, []);
+
+  const connectWallet = async () => {
+    try {
+      if (!ethereum) {
+        sethaveMetamask(false);
+      }
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+      let balance = await provider.getBalance(accounts[0]);
+      let bal = ethers.utils.formatEther(balance);
+      setAccountAddress(accounts[0]);
+      setAccountBalance(bal);
+      setIsConnected(true);
+    } catch (error) {
+      setIsConnected(false);
+    }
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {haveMetamask ? (
+          <div className="App-header">
+            {isConnected ? (
+              <div className="card">
+                <div className="card-row">
+                  <h3>Wallet Address:</h3>
+                  <p>
+                    {accountAddress.slice(0, 4)}...
+                    {accountAddress.slice(38, 42)}
+                  </p>
+                </div>
+                <div className="card-row">
+                  <h3>Wallet Balance:</h3>
+                  <p>{accountBalance}</p>
+                </div>
+              </div>
+            ) : (
+              <p>All is well</p>
+            )}
+            {isConnected ? (
+              <p className="info">🎉 Connected Successfully</p>
+            ) : (
+              <button className="btn" onClick={connectWallet}>
+                Connect
+              </button>
+            )}
+          </div>
+        ) : (
+          <p>Please Install MataMask</p>
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default MetaBtn;
