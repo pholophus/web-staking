8756b6b0b30b4b5ff85ba2dff7adb40317362925
wansidqi <wanahmadsidqi@gmail.com>
2023-01-31 17:19:38 +0800

    Update List.tsx

--- src/components/dashboard/List.tsx
+++ src/components/dashboard/List.tsx
@@ -1,150 +1,184 @@
 import React, { useEffect, useState } from "react";
 import MetaBtn from "../metamask/metamask-btn";
-import {readSC, activeSC, unactiveSC, myFarm, poolEndTime, APR, totalStakePool, percentagePool} from "../../services";
-import { ethers } from 'ethers';
+import {
+  readSC,
+  activeSC,
+  unactiveSC,
+  myFarm,
+  poolEndTime,
+  APR,
+  totalStakePool,
+  percentagePool,
+} from "../../services";
+import { ethers } from "ethers";
 import listSCJson from "../../data/oasis-smart-contract.json";
-import {SC as SCClass} from "../../interface/index";
+import { SC as SCClass } from "../../interface/index";
 
 const List = () => {
-    const [listSC, setListSC] = useState<SCClass[]>([]);
-    const [filteredSC, setFilteredSC] = useState<SCClass[]>([]);
-    const [endPool, setEndPool] = useState<any[]>([]);
-    const [APRValue, setAPRValue] = useState<any[]>([]);
-    const [totalStake, setTotalStake] = useState<any[]>([]);
-    const [percentagePoolValue, setPercentagePoolValue] = useState<any[]>([]);
-    const [poolStatus, setPoolStatus] = useState("unactive");
+  const [listSC, setListSC] = useState<SCClass[]>([]);
+  const [filteredSC, setFilteredSC] = useState<SCClass[]>([]);
+  const [endPool, setEndPool] = useState<any[]>([]);
+  const [APRValue, setAPRValue] = useState<any[]>([]);
+  const [totalStake, setTotalStake] = useState<any[]>([]);
+  const [percentagePoolValue, setPercentagePoolValue] = useState<any[]>([]);
+  const [poolStatus, setPoolStatus] = useState("unactive");
 
-    useEffect(() => {
+  useEffect(() => {
+    readSC().then((res) => {
+      setListSC(res);
+      filterPool(res);
+    });
+  }, [poolStatus]);
 
-        readSC().then((res) => {
-            setListSC(res);
-            filterPool(res);
+  const filterPool = async (listSC: SCClass[]) => {
+    /*reset value*/
+    setEndPool([]);
+    setAPRValue([]);
+    setTotalStake([]);
+    setPercentagePoolValue([]);
+
+    switch (poolStatus) {
+      case "active":
+        activeSC(listSC).then(async (resp: SCClass[]) => {
+          console.log("active resp", resp);
+          getPoolDetail(resp);
         });
-    }, [poolStatus])
-
-    const filterPool = async (listSC: SCClass[]) => {
-
-        /*reset value*/
-        setEndPool([]);
-        setAPRValue([]);
-        setTotalStake([]);
-        setPercentagePoolValue([]);
-
-        switch(poolStatus){
-            case "active":
-                activeSC(listSC).then(async (resp: SCClass[]) => {
-                    console.log("active resp", resp)
-                    getPoolDetail(resp)
-                });
-                break;
-            case "unactive":
-                unactiveSC(listSC).then(async (resp: SCClass[]) => {
-                    console.log("unactive resp", resp)
-                    getPoolDetail(resp)
-                });
-                break;
-            case "myFarm":
-                myFarm(listSC).then((resp) => {
-                    console.log("my resp", resp)
-                    getPoolDetail(resp)
-                });
-                break;
-        }
+        break;
+      case "unactive":
+        unactiveSC(listSC).then(async (resp: SCClass[]) => {
+          console.log("unactive resp", resp);
+          getPoolDetail(resp);
+        });
+        break;
+      case "myFarm":
+        myFarm(listSC).then((resp) => {
+          console.log("my resp", resp);
+          getPoolDetail(resp);
+        });
+        break;
     }
+  };
 
-    const getPoolDetail = async (resp: SCClass[]) => {
+  const getPoolDetail = async (resp: SCClass[]) => {
+    setFilteredSC(resp);
 
-        setFilteredSC(resp)
+    for (const sc of resp) {
+      await poolEndTime(sc).then((resp) => {
+        console.log("time -> ", resp);
+        setEndPool((endPool) => [...endPool, resp]);
+      });
 
-        for(const sc of resp){
-            await poolEndTime(sc).then((resp) => {
-                console.log("time -> ", resp)
-                setEndPool(endPool => [...endPool, resp])
-            })
+      await APR(sc).then((resp) => {
+        console.log("APR -> ", resp);
+        setAPRValue((APRValue) => [...APRValue, resp]);
+      });
 
-            await APR(sc).then((resp) => {
-                console.log("APR -> ", resp)
-                setAPRValue(APRValue => [...APRValue, resp])
-            });
+      await totalStakePool(sc).then((resp) => {
+        console.log("total stake pool -> ", resp);
+        setTotalStake((totalStake) => [...totalStake, resp]);
+      });
 
-            await totalStakePool(sc).then((resp) => {
-                console.log("total stake pool -> ", resp)
-                setTotalStake(totalStake => [...totalStake, resp])
-            });
-
-            await percentagePool(sc).then((resp) => {
-                console.log("percentage pool -> ", resp)
-                setPercentagePoolValue(percentagePoolValue => [...percentagePoolValue, resp])
-            })
-        }
+      await percentagePool(sc).then((resp) => {
+        console.log("percentage pool -> ", resp);
+        setPercentagePoolValue((percentagePoolValue) => [
+          ...percentagePoolValue,
+          resp,
+        ]);
+      });
     }
+  };
 
-    return (
-        <>
-            <div className="flex flex-col">
-                <div className="overflow-x-auto">
-                    <div className="p-1.5 w-full inline-block align-middle">
-                        <div className="overflow-hidden border rounded-lg">
-                            <table className="min-w-full divide-y divide-gray-200 bg-[#212121]">
-                                <tbody className="divide-y divide-gray-200">
-                                {filteredSC.map((sc, index) => 
-                                    <tr key={index}>
-                                        <td className="px-6 py-4 text-sm font-medium text-[#ffffff] whitespace-nowrap">
-                                            <div className="flex">
-                                                OASIS {listSCJson[sc.index].days} Days
-                                            </div>
-                                            <div className="flex">
-                                                V {listSCJson[sc.index].ver}
-                                            </div>
-                                        </td>
-                                        <td className="px-6 py-4 text-sm text-[#ffffff] whitespace-nowrap">
-                                            <div className="flex">
-                                                <p className="">End</p>
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
-                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
-                                                </svg>
-                                            </div>
-                                            <div className="flex">
-                                                {endPool[index]}
-                                            </div>
-                                        </td>
-                                        <td className="px-6 py-4 text-sm text-[#ffffff] whitespace-nowrap">
-                                            <div className="flex">
-                                                <p className="">APR</p>
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
-                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
-                                                </svg>
-                                            </div>
-                                            <div className="flex">
-                                                {APRValue[index]}
-                                            </div>
-                                        </td>
-                                        <td className="px-6 py-4 text-sm font-medium text-[#ffffff] whitespace-nowrap">
-                                            <div className="flex">
-                                                <p className="">Total Staked</p>
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
-                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
-                                                </svg>
-                                            </div>
-                                            <div className="flex">
-                                                ${totalStake[index]}
-                                            </div>
-                                        </td>
-                                        <td className="px-6 py-4 text-sm font-medium text-[#ffffff] whitespace-nowrap">
-                                            <div className="flex">
-                                                <p className="">{percentagePoolValue[index]}%</p>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    )}
-                                </tbody>
-                            </table>
+  return (
+    <>
+      <div className="flex flex-col">
+        <div className="overflow-x-auto">
+          <div className="p-1.5 w-full inline-block align-middle">
+            <div className="overflow-hidden border rounded-lg">
+              <table className="min-w-full divide-y divide-gray-200 bg-[#212121]">
+                <tbody className="divide-y divide-gray-200">
+                  {filteredSC.map((sc, index) => (
+                    <tr key={index}>
+                      <td className="px-6 py-4 text-sm font-medium text-[#ffffff] whitespace-nowrap">
+                        <div className="flex">
+                          OASIS {listSCJson[sc.index].days} Days
                         </div>
-                    </div>
-                </div>
+                        <div className="flex">V {listSCJson[sc.index].ver}</div>
+                      </td>
+                      <td className="px-6 py-4 text-sm text-[#ffffff] whitespace-nowrap">
+                        <div className="flex">
+                          <p className="">End</p>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="w-5 h-5"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"
+                            />
+                          </svg>
+                        </div>
+                        <div className="flex">{endPool[index]}</div>
+                      </td>
+                      <td className="px-6 py-4 text-sm text-[#ffffff] whitespace-nowrap">
+                        <div className="flex">
+                          <p className="">APR</p>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="w-5 h-5"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"
+                            />
+                          </svg>
+                        </div>
+                        <div className="flex">{APRValue[index]}</div>
+                      </td>
+                      <td className="px-6 py-4 text-sm font-medium text-[#ffffff] whitespace-nowrap">
+                        <div className="flex">
+                          <p className="">Total Staked</p>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="w-5 h-5"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"
+                            />
+                          </svg>
+                        </div>
+                        <div className="flex">${totalStake[index]}</div>
+                      </td>
+                      <td className="px-6 py-4 text-sm font-medium text-[#ffffff] whitespace-nowrap">
+                        <div className="flex">
+                          <p className="">{percentagePoolValue[index]}%</p>
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
             </div>
-        </>
-    );
-}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default List;
