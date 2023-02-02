6ddeb0dab864c33bfb823c3270399dbfddc38ce4
wansidqi <wanahmadsidqi@gmail.com>
2023-02-02 11:52:37 +0800

    Update Modal.tsx

--- src/components/dashboard/Modal.tsx
+++ src/components/dashboard/Modal.tsx
@@ -1,6 +1,42 @@
 import React, { useState } from "react";
 
 const Modal = ({ setShowModal }: any) => {
+  const [stake, setStake] = useState({
+    option: "block",
+    detail: "hidden",
+  });
+
+  const onStakeClicked = () => {
+    setStake({
+      option: "hidden",
+      detail: "block",
+    });
+  };
+
+  const onCancelStake = () => {
+    setStake({
+      option: "block",
+      detail: "hidden",
+    });
+  };
+
+  const closeX = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+  );
+
   return (
     <div>
       <div className="fixed bottom-0 inset-x-0 px-4 pb-4 sm:inset-0 sm:flex sm:items-center sm:justify-center">
@@ -14,39 +50,64 @@ const Modal = ({ setShowModal }: any) => {
           aria-modal="true"
           aria-labelledby="modal-headline"
         >
-          <div className="px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-            <div className="sm:flex sm:items-start">
-              <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                <svg
-                  className="h-6 w-6 text-red-600"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-                  />
-                </svg>
-              </div>
-              <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                <h3
-                  className="text-lg leading-6 font-medium text-gray-900"
-                  id="modal-headline"
-                >
-                  Modal Title
-                </h3>
-                <div className="mt-2">
-                  <p className="text-sm leading-5 text-gray-500">
-                    Modal content goes here.
-                  </p>
+          <div className={stake.option}>
+            <div className="px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+              <div className="">
+                <div className="text-center ml-4">
+                  <h3 className="text-lg leading-6 font-medium text-gray-900 my-4">
+                    Your $Oasis Staked:
+                    <span> 10,000</span>
+                  </h3>
+                  <div className="mt-2">
+                    <div className="mt-8 px-20 flex justify-between">
+                      <button
+                        onClick={onStakeClicked}
+                        className="bg-green-600 w-[120px] py-2 rounded-lg font-medium"
+                      >
+                        STAKE
+                      </button>
+                      <button className="bg-red-600 w-[120px] py-2 rounded-lg font-medium">
+                        UNSTAKE
+                      </button>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
+
+          <div className={stake.detail}>
+            <div className="px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+              <div className="">
+                <div className="text-center">
+                  <div className="flex justify-between">
+                    <h3 className="text-lg leading-6 font-medium text-gray-900 border-b border-black  ml-4">
+                      STAKE OASIS
+                    </h3>
+                    <button onClick={onCancelStake}>{closeX}</button>
+                  </div>
+                  <div className="flex justify-between px-8 my-4 border border-gray-600 rounded-xl">
+                    <div className="my-6">
+                      <p className="leading-5">Stake:</p>
+                      <p className="leading-5 text-gray-500">0</p>
+                    </div>
+                    <div className="mt-6">
+                      <p className="leading-5">
+                        Balance: <span>0</span>{" "}
+                      </p>
+                      <button className="leading-5 text-sm text-gray-500">
+                        MAX
+                      </button>
+                    </div>
+                  </div>
+                  <button className="bg-yellow-400 rounded-xl px-20 py-2 text-center">
+                    CONFIRM
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
           <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
             <button
               className="py-2 px-4 border border-gray-300 rounded-md text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out"
